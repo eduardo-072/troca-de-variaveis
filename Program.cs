958c6e8d00419367edbca5fec0033f3afa143bd5
eduardo-072 <eduardosilvaaa072@gmail.com>
2023-09-02 02:20:38 -0300

    alterei as variáveis

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
-﻿Console.WriteLine("tremendo de frio");
-Console.WriteLine("no asfalto negro da rua");
-Console.WriteLine("a criança chora");
+﻿string nome1, auxiliar;
+string nome2;
+
+Console.WriteLine("troca de nomes \n");
+
+Console.Write("nome1 = ");
+nome1 = Console.ReadLine()!;
+
+Console.Write("nome2 = ");
+nome2 = Console.ReadLine()!;
+
+auxiliar = nome1;
+nome1 = nome2;
+nome2 = auxiliar;
+
+Console.WriteLine($"nome1 = {nome1}");
+Console.WriteLine($"nome2 = {nome2}");
+
